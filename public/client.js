cedbfb7ae53dd192083aa4793849187ac84d7db4
NIsarg <nisarg@NIsargs-MacBook-Air.local>
2023-06-20 11:11:59 +0530

    tada:Init Added code from passkey tut

---
+++ public/client.js
@@ -0,0 +1,91 @@
+/*
+ * @license
+ * Copyright 2023 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+export const $ = document.querySelector.bind(document);
+
+export async function _fetch(path, payload = '') {
+  const headers = {
+    'X-Requested-With': 'XMLHttpRequest',
+  };
+  if (payload && !(payload instanceof FormData)) {
+    headers['Content-Type'] = 'application/json';
+    payload = JSON.stringify(payload);
+  }
+  const res = await fetch(path, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: headers,
+    body: payload,
+  });
+  if (res.status === 200) {
+    // Server authentication succeeded
+    return res.json();
+  } else {
+    // Server authentication failed
+    const result = await res.json();
+    throw new Error(result.error);
+  }
+};
+
+export const base64url = {
+  encode: function(buffer) {
+    const base64 = window.btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+  },
+  decode: function(base64url) {
+    const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+    const binStr = window.atob(base64);
+    const bin = new Uint8Array(binStr.length);
+    for (let i = 0; i < binStr.length; i++) {
+      bin[i] = binStr.charCodeAt(i);
+    }
+    return bin.buffer;
+  }
+}
+
+class Loading {
+  constructor() {
+    this.progress = $('#progress');
+  }
+  start() {
+    this.progress.indeterminate = true;
+    const inputs = document.querySelectorAll('input');
+    if (inputs) {
+      inputs.forEach(input => input.disabled = true);
+    }
+  }
+  stop() {
+    this.progress.indeterminate = false;
+    const inputs = document.querySelectorAll('input');
+    if (inputs) {
+      inputs.forEach(input => input.disabled = false);
+    }
+  }
+}
+
+export const loading = new Loading();
+
+// TODO: Add an ability to create a passkey: Create the registerCredential() function.
+
+// TODO: Add an ability to authenticate with a passkey: Create the authenticate() function.
+
+export async function updateCredential(credId, newName) {
+  return _fetch(`/auth/renameKey`, { credId, newName });
+}
+
+export async function unregisterCredential(credId) {
+  return _fetch(`/auth/removeKey?credId=${encodeURIComponent(credId)}`);
+};
